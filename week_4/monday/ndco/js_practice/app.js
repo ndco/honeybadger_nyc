706fe3e31593748dbb287c851a92dabd93792a27
Andy Seo <sbki99@gmail.com>
2017-12-03 11:57:43 -0500

    week4 Sun

--- week_4/monday/ndco/js_practice/app.js
+++ week_4/monday/ndco/js_practice/app.js
@@ -1,4 +1,11 @@
-var statement = 'HELLO WORLD';
+let listNum = prompt('place numbers');
+arrayNum = listNum.split(',');
+
+
+
+
+
+// var statement = 'HELLO WORLD';
 
 // MAKE ALL CHAR UPPERCASE
 // MAKE ALL CHAR LOWERCASE
