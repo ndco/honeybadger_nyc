7d5fe7c75e55ecedf6e3b465a30ab3a807268e6b
Andy Seo <sbki99@gmail.com>
2017-12-06 18:39:46 -0500

    week 5 wed commit

--- week_5/monday/ndco/js_functions/app.js
+++ week_5/monday/ndco/js_functions/app.js
@@ -1,102 +1,136 @@
-function func (string) {
-    console.log(string.toUpperCase());
-};
+//Round 0
+var words = ["hello", "what", "is", "up", "dude"]
 
-function doSomethingNtimes (func, num) {
-    let i = 0;
-    while (i <= num) {
-        func();
-        i ++;
-    };
-};
-
-
-
-
-
-
-
-// WRITE A FUNCTION CALLED bigOrSmall it accepts 1 param and console logs "this number is big" if the number is over 10 and "this number is small" otherwise
-function bigOrSmall (num) {
-    if (num > 10) {
-        console.log('this num is big');
-    } else {
-        console.log('this num is small');
-    };
-};
-
-
-//Write a function oddOrEven accepts one argument and console logs "this number is odd" if the number is odd and "this number is even" if the number is even
-function oddOrEven (num) {
-    result = num % 2;
-    if (result === 0) {
-        console.log('this num is even');
-    } else {
-        console.log('this num is odd');
-    };
-};
-
-// num % 2 === 0 ? console.log('this num is even') : console.log('this is odd');
-
-// Write a function call embiggen that accepts one argument, a string, and returns a CAPS LOCKED version of that string
-function embiggen(str) {
-    return str.toUpperCase();
-};
-
-
-// Write a function called reverser that accepts on argument, a string, and returns a reversed version of that string
-function reverser (str) {
-    console.log(str);
-    strArray = str.split('');
-    console.log(strArray);
-    strBckArray = strArray.reverse();
-    newStr = strBckArray.join('');
-
-    return newStr;
+function lengths (array) {
+    for (i = 0; i < array.length; i ++) {
+        console.log(array);
+        let wordLengths = [array[i].length.push];
+        console.log(wordLengths)
+        return wordLengths;
+    }
 }
 
 
 
-/*
-############################################################################
-var thisWeekTimes = [52, 51, 53, 52, 54];
-var lastWeekTimes = [54, 57, 58, 53, 54, 58];
-var weekBeforeTimes = [100, 102, 142, 133, 123, 141, 120];
 
 
-function weekSum (numLists) {
-    console.log(numLists);
-    let newNums = numLists;
-    numArray = newNums.split(',');
-    sum = 0;
-    for (var i = 0; i < numArray.length; i++) {
-        sum += numArray[i];
-    };
-    var avg = sum / numArray.length;
-    console.log = sum;
-    console.log = avg;
-};
 
 
-// var sum = 0;
-// for (var i = 0; i < thisWeekTimes.length; i++) {
-//     sum += thisWeekTimes[i];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function func (string) {
+//     console.log(string.toUpperCase());
+// };
+
+// function doSomethingNtimes (func, num) {
+//     let i = 0;
+//     while (i <= num) {
+//         func();
+//         i ++;
+//     };
+// };
+
+
+
+// // WRITE A FUNCTION CALLED bigOrSmall it accepts 1 param and console logs "this number is big" if the number is over 10 and "this number is small" otherwise
+// function bigOrSmall (num) {
+//     if (num > 10) {
+//         console.log('this num is big');
+//     } else {
+//         console.log('this num is small');
+//     };
+// };
+
+
+// //Write a function oddOrEven accepts one argument and console logs "this number is odd" if the number is odd and "this number is even" if the number is even
+// function oddOrEven (num) {
+//     result = num % 2;
+//     if (result === 0) {
+//         console.log('this num is even');
+//     } else {
+//         console.log('this num is odd');
+//     };
+// };
+
+// // num % 2 === 0 ? console.log('this num is even') : console.log('this is odd');
+
+// // Write a function call embiggen that accepts one argument, a string, and returns a CAPS LOCKED version of that string
+// function embiggen(str) {
+//     return str.toUpperCase();
+// };
+
+
+// // Write a function called reverser that accepts on argument, a string, and returns a reversed version of that string
+// function reverser (str) {
+//     console.log(str);
+//     strArray = str.split('');
+//     console.log(strArray);
+//     strBckArray = strArray.reverse();
+//     newStr = strBckArray.join('');
+
+//     return newStr;
 // }
-// var thisWeekAvg = sum / thisWeekTimes.length;
 
-// var sum = 0;
-// for (var i = 0; i < lastWeekTimes.length; i++) {
-//     sum += lastWeekTimes[i];
-// }
-// var lastWeekAvg = sum / lastWeekTimes.length;
 
-// var sum = 0;
-// for (var i = 0; i < weekBeforeTimes.length; i++) {
-//     sum += weekBeforeTimes[i];
-// }
-// var weekBeforeAvg = sum / weekBeforeTimes.length;
 
-// console.log(thisWeekAvg);
-// console.log(lastWeekAvg);
-// console.log(weekBeforeAvg);
-*/
+//
+// ############################################################################
+// var thisWeekTimes = [52, 51, 53, 52, 54];
+// var lastWeekTimes = [54, 57, 58, 53, 54, 58];
+// var weekBeforeTimes = [100, 102, 142, 133, 123, 141, 120];
+
+
+// function weekSum (numLists) {
+//     console.log(numLists);
+//     let newNums = numLists;
+//     numArray = newNums.split(',');
+//     sum = 0;
+//     for (var i = 0; i < numArray.length; i++) {
+//         sum += numArray[i];
+//     };
+//     var avg = sum / numArray.length;
+//     console.log = sum;
+//     console.log = avg;
+// };
+
+
+// // var sum = 0;
+// // for (var i = 0; i < thisWeekTimes.length; i++) {
+// //     sum += thisWeekTimes[i];
+// // }
+// // var thisWeekAvg = sum / thisWeekTimes.length;
+
+// // var sum = 0;
+// // for (var i = 0; i < lastWeekTimes.length; i++) {
+// //     sum += lastWeekTimes[i];
+// // }
+// // var lastWeekAvg = sum / lastWeekTimes.length;
+
+// // var sum = 0;
+// // for (var i = 0; i < weekBeforeTimes.length; i++) {
+// //     sum += weekBeforeTimes[i];
+// // }
+// // var weekBeforeAvg = sum / weekBeforeTimes.length;
+
+// // console.log(thisWeekAvg);
+// // console.log(lastWeekAvg);
+// // console.log(weekBeforeAvg);
+//
