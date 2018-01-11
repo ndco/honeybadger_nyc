7a314d2ec392a784abcb8cfb166121e4e478542f
Andy Seo <sbki99@gmail.com>
2018-01-10 19:59:22 -0500

    jan 10 wed commit

--- homeworks/homework2/app.js
+++ homeworks/homework2/app.js
@@ -3,14 +3,25 @@ window.onload = function() {
     //Create an HTML page with two buttons that argue with each other. When one button is clicked, the text "I'm right" should be placed next to it. When the other button is clicked, the text is replaced with, "No, I'm right!"
 
     const buttons = document.querySelectorAll('.exercise button');
-    console.log(buttons)
+    console.log(buttons);
+
     const largeElemet = document.getElementById('myElement');
+
+    const myText = document.getElementsByClassName('3');
+
+    const mySubmit = document.getElementById('submit');
+
 
     for (i = 0; i < buttons.length; i ++) {
         addTextListener(buttons[i]);
     }
-
+ 
     addHoverListener(largeElemet);
+
+    addKeyPressListner(myText);
+
+    addUserValidationListener(submit);
+
 
     function addTextListener (node) {
         node.addEventListener('click', function(e) {
@@ -46,8 +57,32 @@ window.onload = function() {
         // - Example: If the user presses "J", the text inside the H1 should be "J".
     function addKeyPressListner (node) {
         node.addEventListener('keypress', function(e) {
+            const keyName = e.key;
+
+            let hText = document.getElementById('emptyHeader');
+            if (hText.innerHTML === undefined) {
+                hText.innerHTML = keyName;
+            } else {
+                hText.innerHTML += keyName;
+            }
+        })
+    }
+
+
+    function addUserValidationListener (node) {
+        node.addEventListener('click', function(e) {
+            const username = document.getElementById('userId').value;
+            const password = document.getElementById('userPswd').value;
+            const email = document.getElementById('userEmail').value;
+
+            if (username ==='' || password ==='' || email ==='' || password.search(/[0-9]/) !== -1) {
+                alert ('login successful!')
+            } else {
+                alert ('incorrect!')
+            }
 
         })
     }
+
 
 };
