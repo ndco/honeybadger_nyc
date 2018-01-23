ba4c6e0b03b05fa72cb0badb5acffec6220516fc
Andy Seo <sbki99@gmail.com>
2018-01-22 20:19:53 -0500

    jan 22 mon 2nd commit

---
+++ week_12/monday/ndco/poke_node_api_starter/app.js
@@ -0,0 +1,24 @@
+// import { STATUS_CODES } from 'http';
+
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const pokemon = require('./data/poke_array');
+
+app.use(logger('dev'));
+
+// INDEX ROUTE FOR THE POKEMON SHOULD SEND ALL THE POKEMON TO THE USER
+app.get('/pokemons', function (req, res) {
+    res.send(pokemon);
+});
+
+// BE ABLE TO SEARCH FOR A POKEMON BY NAME
+'/pokemons/byName/:name'
+// BE ABLE TO SEARCH FOR A POKEMON BY ID
+// BE ABLE TO SEARCH FOR A POKEMON BY TYPE
+'/pokemons/byType/:type'
+// BE ABLE TO GET A RANDOM POKEMON
+'/pokemons/random'
+'/pokemons/:id'
+
+app.listen(3000);
